b367ae49e970f3b568c139686ea1c8089476c1b6
purple63 <purple63@email.com>
2023-08-30 05:41:18 +0500

    task#001 Add class: Quadcopter [quadcopter/base.py] Add packages: api, interfaces, quadcopter Define inits in packages: src, api, interfaces, quadcopter Add point of entry: main.py Add library 'djitellopy' in requirements.txt

--- src/quadcopter/base.py
+++ src/quadcopter/base.py
@@ -1,20 +1,24 @@
-from api import TelloAPI
+from src.api import TelloAPI
+from typing import Any
 
 
 class Quadcopter(object):
     """
-    Класс Quadcopter предназначен для создания экземпляров классов из пакета api.
+    Класс Quadcopter предназначен для создания экземпляров классов из пакета src/api.
     Созданные экземпляры представляют собой API соответствующих моделей квадрокоптеров.
     В данный момент доступы следующие модели: "Trello".
     Класс Quadcopter является классом-фабрикой и не может быть инстанцирован.
     """
+    __AVAILABLE_MODELS = {
+        'Tello': TelloAPI
+    }
+
     def __new__(
             cls,
             model: str,
             ip: str
-    ):
-        match model:
-            case 'Tello':
-                return TelloAPI(ip)
-            case _:
-                raise NameError(f'«{model}» model not available')
+    ) -> Any:
+        if model not in cls.__AVAILABLE_MODELS:
+            raise NameError(f'«{model}» model is not available')
+
+        return cls.__AVAILABLE_MODELS[model].__call__(ip)
